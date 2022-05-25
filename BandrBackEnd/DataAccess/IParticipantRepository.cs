8eb04fc3fa6d76c03f3235cecb5b2aaad56c3ac3
Madden <maddenpurcell@gmail.com>
2022-05-24 19:54:38 -0500

    Completed Participant Data setup.

---
+++ BandrBackEnd/DataAccess/IParticipantRepository.cs
@@ -0,0 +1,13 @@
+﻿using BandrBackEnd.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace BandrBackEnd.DataAccess
+{
+    public interface IParticipantRepository
+    {
+        public Participant GetParticipantById(int id);
+        public void CreateParticipant(Participant participant);
+        public void DeleteParticipant(int id);
+    }
+}
